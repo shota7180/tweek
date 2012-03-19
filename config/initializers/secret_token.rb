a5d32e7b6e8e13507126b19978fd4f657f5bce38
Shota Kiuchi <shota7180@gmail.com>
2012-03-20 01:23:11 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweek::Application.config.secret_token = '43c04c52d9d5756cb03ee26a626b264de29915c5710fee87d848156ca833859390a5a4625f6d8b6c09a32419c92484543050893804ba3a03e5299e9752b85939'
