94783fcc596b7950cd1ffa4ef0a426aba24184b2
Shota Kiuchi <shota7180@gmail.com>
2012-03-20 01:55:50 +0900

    add top

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,4 @@
+class TopController < ApplicationController
+  def recent
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Tweek::Application.routes.draw do
+  get "top/recent"  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +50,7 @@ Tweek::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => "top#recent"  
 
   # See how all your routes lay out with "rake routes"
 
